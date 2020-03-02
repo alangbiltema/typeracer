fe422e21e14af8bc2d53647ada4c6b1d8eff78b2
Alfred Längheim <alfred.langheim@gmail.com>
2020-03-02 09:28:57 +0100

    text

--- typefast.py
+++ typefast.py
@@ -5,3 +5,24 @@ import random
 import sys
 
 
+class Game:
+
+     def __init__(self):
+        self.w=750
+        self.h=500
+        self.reset=True
+        self.active = False
+        self.input_text=''
+        self.word = ''
+        self.time_start = 0
+        self.total_time = 0
+        self.accuracy = '0%'
+        self.results = 'Time:0 Accuracy:0 % Wpm:0 '
+        self.wpm = 0
+        self.end = False
+        self.HEAD_C = (255,213,102)
+        self.TEXT_C = (240,240,240)
+        self.RESULT_C = (255,70,70)
+
+        pygame.init():
+        
