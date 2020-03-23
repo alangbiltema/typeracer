f390138d525870ac3f3d4fcaac4b0424e21dffb1
Alfred Längheim <alfred.langheim@gmail.com>
2020-03-23 10:18:29 +0100

    Meningar, mm.

--- typefast.py
+++ typefast.py
@@ -38,11 +38,27 @@ class Game:
          return sentence
 
       def show_results(self, screen):
-         #Calculate Time
-         #Calculate Accuracy
-         #Calculate WPM
+         if(not self.end):
+            #Calculate Time
+            self.total_time = time.time() - self.time_start
+ 
+            #Calculate Accuracy
+            count = 0
+            for i,c in enumerate(self.word):
+               try:
+                  if self.input_text[i] == c:
+                     count += 1
+               except:
+                  pass
+            self.accuracy = count/len(self.word)*100
+            
+            #Calculate WPM
+            self.WPM = len(self.input_text)*60/(5*self.total_time)
+            self.end = True
+            print(self.total_time)
 
-
+            self.results = 'Time:'+str(round(self.total_time)) +" secs Accuracy:"+ str(round(self.accuracy)) + "%" + ' Wpm: ' + str(round(self.wpm))
+            
       def run(self):
          self.reset_game()
 
@@ -62,4 +78,34 @@ class Game:
                   x,y = pygame.mouse.get_pos()
         
       def reset_game():
-         
+         self.screen.blit(self.open_img, (0,0))
+
+         pygame.display.update()
+         time.sleep(1)
+
+         self.reset=False
+         self.end=False
+
+         self.input_text=''
+         self.word = ''
+         self.word_start = 0 
+         self.total_time = 0
+         self.wpm = 0
+
+         self.word = self.get_sentence()
+         if (not self.word) : self.reset_game()
+
+         self.screen.fill((0,0,0))
+         self.screen.blit(self.background,(0,0))
+         msg = ""
+         self.draw_text(self.screen, msg, 80, 80, self.HEAD_C)
+
+         pygame.draw.rect(self.screen,(255,192,25),(50,250,650,50),2)
+
+         self.draw_text(self.screen, self.word, 200, 28, self.TEXT_C)
+
+         pygame.display.update()
+
+
+
+Game().run()
